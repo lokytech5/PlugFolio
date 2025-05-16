a99321ed41e88e8dd12040a0d36d9862db728c8c
lokytech5 <lokytech5@gmail.com>
2025-05-16 17:02:54 +0100

    update user-data script

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -2,20 +2,32 @@
 exec > /var/log/user-data.log 2>&1
 set -x
 
-# Update the package index
+# Update and upgrade packages
 sudo apt update -y && sudo apt upgrade -y
 
-# Install required packages
-sudo apt install -y awscli docker.io git nginx
+# Install essentials
+sudo apt install -y python3-pip git
 
-# Configure Docker
-sudo systemctl enable docker && sudo systemctl start docker
+# Install AWS CLI
+pip3 install awscli --upgrade --user
+export PATH=$PATH:/home/ubuntu/.local/bin
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo systemctl enable docker
+sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Install Nginx
+sudo apt install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
 # Add 2GB Swap
 sudo fallocate -l 2G /swapfile
