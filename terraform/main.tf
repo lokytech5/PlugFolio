b4f8b8bf5b9ffa7a1eac2829b5a2c0bee6ee9f9d
lokytech5 <lokytech5@gmail.com>
2025-05-16 18:16:25 +0100

    use template file for user-data script by adding .tmpl extension to file name and update main.tf to use the template and refernce app service in instance resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,11 +84,13 @@ resource "aws_security_group" "plugfolio_sg" {
 
 # EC2 Instance
 resource "aws_instance" "plugfolio_instance" {
-  ami                         = "ami-084568db4383264d4"
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.plugfolio_sg.id]
-  user_data                   = file("../scripts/user-data.sh")
+  ami                    = "ami-084568db4383264d4"
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.plugfolio_sg.id]
+  user_data = templatefile("../scripts/user-data.sh.tmpl", {
+    my_app_service = file("../services/plugfolio-app.service")
+  })
   key_name                    = data.aws_key_pair.existing.key_name
   associate_public_ip_address = true
   tags = {
