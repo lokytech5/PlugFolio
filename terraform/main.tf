89531c24bc05989129caa12eff4aa4077acb75e4
lokytech5 <lokytech5@gmail.com>
2025-05-16 16:48:01 +0100

    update instance resource by adding key pair and associating it with a public ip address and use exiting key pair in dev account. Added ec2 ip address to output file

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,11 +84,13 @@ resource "aws_security_group" "plugfolio_sg" {
 
 # EC2 Instance
 resource "aws_instance" "plugfolio_instance" {
-  ami             = "ami-084568db4383264d4"
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.public.id
-  security_groups = [aws_security_group.plugfolio_sg.id]
-  user_data       = file("../scripts/user-data.sh")
+  ami                         = "ami-084568db4383264d4"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.plugfolio_sg.id]
+  user_data                   = file("../scripts/user-data.sh")
+  key_name                    = data.aws_key_pair.existing.key_name
+  associate_public_ip_address = true
   tags = {
     Name = "plugfolio-instance"
   }
@@ -98,4 +100,9 @@ resource "aws_instance" "plugfolio_instance" {
 data "aws_route53_zone" "main" {
   name         = var.root_domain
   private_zone = false
+}
+
+#Fetch existing Key Pair from AWS
+data "aws_key_pair" "existing" {
+  key_name = "ec2-login-key"
 }
