8c798963ea6f49b03b858928069855920bcec24d
lokytech5 <lokytech5@gmail.com>
2025-05-20 21:55:09 +0100

    update iam policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,7 +145,7 @@ resource "random_string" "suffix" {
   numeric = true
 }
 
-# S3 Bucket
+# S3 Bucket for script
 resource "aws_s3_bucket" "plugfolio_scripts" {
   bucket = "plugfolio-scripts${random_string.suffix.result}"
   tags = {
