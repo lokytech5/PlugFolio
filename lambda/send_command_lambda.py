373fd5b8e25679889e8235b91909fe59e3dfca9b
lokytech5 <lokytech5@gmail.com>
2025-05-29 02:53:32 +0100

    fix send-command lambda parameter to determine the actual parameter type

--- lambda/send_command_lambda.py
+++ lambda/send_command_lambda.py
@@ -10,17 +10,18 @@ def default_serializer(obj):
 def lambda_handler(event, context):
     ssm = boto3.client('ssm')
 
+    parameters = event['Parameters']
+
     response = ssm.send_command(
         InstanceIds=event['InstanceIds'],
         DocumentName=event['DocumentName'],
         Parameters={
-            'RepoUrl': [event['RepoUrl']],
-            'DockerImageRepo': [event['DockerImageRepo']],
-            'DockerImageTag': [event['DockerImageTag']],
-            'Subdomain': [event['Subdomain']],
-            'BucketName': [event['BucketName']]
+            'RepoUrl': parameters['RepoUrl'],
+            'DockerImageRepo': parameters['DockerImageRepo'],
+            'DockerImageTag': parameters['DockerImageTag'],
+            'Subdomain': parameters['Subdomain'],
+            'BucketName': parameters['BucketName']
         }
     )
 
-    # Serialize and return
     return json.loads(json.dumps(response, default=default_serializer))
